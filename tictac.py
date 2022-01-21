139000754a2c713e58053e3f5bcef50731e3323b
7000words <emekadrake707@gmail.com>
2022-01-21 21:49:37 +0000

    improved

--- tictac.py
+++ tictac.py
@@ -1,120 +1,96 @@
 tictac = ["0","0","0","0","0","0","0","0","0"]
 
-print(tictac[0],tictac[1],tictac[2])
-print(tictac[3],tictac[4],tictac[5])
-print(tictac[6],tictac[7],tictac[8])
+def tictactoe():
+    print(tictac[0],tictac[1],tictac[2])
+    print(tictac[3],tictac[4],tictac[5])
+    print(tictac[6],tictac[7],tictac[8])
+tictactoe()
 
-for x in range(9):
+print("""
+topleft, topmiddle, topright
+left, middle, right
+bottomleft, bottommiddle, bottomright
+""")
+
+for c in range(5):
     x  = input("where do you want to place(x): ")
     
-
     if x == "topright":
         tictac[2]= "x"
-        print(tictac[0],tictac[1],tictac[2])
-        print(tictac[3],tictac[4],tictac[5])
-        print(tictac[6],tictac[7],tictac[8])
+        tictactoe()
     
     elif x == "topleft":
         tictac[0]= "x"
-        print(tictac[0],tictac[1],tictac[2])
-        print(tictac[3],tictac[4],tictac[5])
-        print(tictac[6],tictac[7],tictac[8])
+        tictactoe()
 
     elif x == "topmiddle":
         tictac[1]= "x"
-        print(tictac[0],tictac[1],tictac[2])
-        print(tictac[3],tictac[4],tictac[5])
-        print(tictac[6],tictac[7],tictac[8])
+        tictactoe()
 
     elif x == "left":
         tictac[3]= "x"
-        print(tictac[0],tictac[1],tictac[2])
-        print(tictac[3],tictac[4],tictac[5])
-        print(tictac[6],tictac[7],tictac[8])
+        tictactoe()
     
     elif x == "middle":
         tictac[4]= "x"
-        print(tictac[0],tictac[1],tictac[2])
-        print(tictac[3],tictac[4],tictac[5])
-        print(tictac[6],tictac[7],tictac[8])
+        tictactoe()
 
     elif x == "right":
         tictac[5]= "x"
-        print(tictac[0],tictac[1],tictac[2])
-        print(tictac[3],tictac[4],tictac[5])
-        print(tictac[6],tictac[7],tictac[8])
+        tictactoe()
     
     elif x == "bottomleft":
         tictac[6]= "x"
-        print(tictac[0],tictac[1],tictac[2])
-        print(tictac[3],tictac[4],tictac[5])
-        print(tictac[6],tictac[7],tictac[8])
+        tictactoe()
     
     elif x == "bottommiddle":
         tictac[7]= "x"
-        print(tictac[0],tictac[1],tictac[2])
-        print(tictac[3],tictac[4],tictac[5])
-        print(tictac[6],tictac[7],tictac[8])
+        tictactoe()
     
     elif x == "bottomright":
         tictac[8]= "x"
-        print(tictac[0],tictac[1],tictac[2])
-        print(tictac[3],tictac[4],tictac[5])
-        print(tictac[6],tictac[7],tictac[8])
+        tictactoe()
+
 
     y  = input("where do you want to place(y): ")
     
     
     if y == "topright":
         tictac[2]= "y"
-        print(tictac[0],tictac[1],tictac[2])
-        print(tictac[3],tictac[4],tictac[5])
-        print(tictac[6],tictac[7],tictac[8])
+        tictactoe()
     
     elif y == "topleft":
         tictac[0]= "y"
-        print(tictac[0],tictac[1],tictac[2])
-        print(tictac[3],tictac[4],tictac[5])
-        print(tictac[6],tictac[7],tictac[8])
+        tictactoe()
 
     elif y == "topmiddle":
         tictac[1]= "y"
-        print(tictac[0],tictac[1],tictac[2])
-        print(tictac[3],tictac[4],tictac[5])
-        print(tictac[6],tictac[7],tictac[8])
+        tictactoe()
 
     elif y == "left":
         tictac[3]= "y"
-        print(tictac[0],tictac[1],tictac[2])
-        print(tictac[3],tictac[4],tictac[5])
-        print(tictac[6],tictac[7],tictac[8])
+        tictactoe()
     
     elif y == "middle":
         tictac[4]= "y"
-        print(tictac[0],tictac[1],tictac[2])
-        print(tictac[3],tictac[4],tictac[5])
-        print(tictac[6],tictac[7],tictac[8])
+        tictactoe()
 
     elif y == "right":
         tictac[5]= "y"
-        print(tictac[0],tictac[1],tictac[2])
-        print(tictac[3],tictac[4],tictac[5])
-        print(tictac[6],tictac[7],tictac[8])
+        tictactoe()
     
     elif y == "bottomleft":
         tictac[6]= "y"
-        print(tictac[0],tictac[1],tictac[2])
-        print(tictac[3],tictac[4],tictac[5])
-        print(tictac[6],tictac[7],tictac[8])
+        tictactoe()
     
     elif y == "bottommiddle":
         tictac[7]= "y" 
-        print(tictac[0],tictac[1],tictac[2])
-        print(tictac[3],tictac[4],tictac[5])
-        print(tictac[6],tictac[7],tictac[8])
+        tictactoe()
     
     elif y == "bottomright":
         tictac[8]= "y"
-        print(tictac[0],tictac[1],tictac[2])
-        print(tictac[3],tictac[4],tictac[5])
-        print(tictac[6],tictac[7],tictac[8])
+        tictactoe()
+
+  
+if x in tictac[0] and tictac[4] and tictac[8]:
+    print("you won")
